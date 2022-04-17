68bce3c407d00dbe007d6443af77f879ac56527e
Victor de Castro <56875087+VictorCastroBR@users.noreply.github.com>
2022-04-17 16:47:53 +0000

    Fixed, erro ao receber valor vazio

--- core/views.py
+++ core/views.py
@@ -3,29 +3,37 @@ from django.contrib import messages
 import requests
 
 def index(request):
+    
     if request.method == 'POST':
+      
         get_cep = request.POST.get('cep')
         if len(get_cep) != 8: 
-            pass
+          messages.error(request, 'CEP inválido!')
         else:
-            address = requests.get(f'https://viacep.com.br/ws/{get_cep}/json/')
+          address = requests.get(f'https://viacep.com.br/ws/{get_cep}/json/')
+          try:
             address = address.json()
-        
-        if not 'erro' in address:
-            context = {
-                'cep': address['cep'],
-                'logradouro': address['logradouro'],
-                'complemento': address['complemento'],
-                'localidade': address['localidade'],
-                'uf': address['uf'],
-                'ibge': address['ibge'],
-                'ddd': address['ddd'],
-            }
-
-        else:
+          except:
             messages.error(request, 'CEP inválido!')
             return redirect ('/')
+        
+          if not 'erro' in address:
+              context = {
+                  'cep': address['cep'],
+                  'logradouro': address['logradouro'],
+                  'complemento': address['complemento'],
+                  'localidade': address['localidade'],
+                  'uf': address['uf'],
+                  'ibge': address['ibge'],
+                  'ddd': address['ddd'],
+              }
+          
+          else:
+              messages.error(request, 'CEP inválido!')
+              
 
-        return render(request, 'index.html', context)
+          return render(request, 'index.html', context)
     else:
         return render(request, 'index.html')
+
+    return redirect ('/')
---
+++ run.sh
@@ -0,0 +1,3 @@
+source venv/bin/activate
+pip3 install django
+python manage.py runserver 0.0.0.0:8000
