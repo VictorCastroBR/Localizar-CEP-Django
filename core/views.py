f7834366e3a2b2b539f4a4fe5161481504360fae
VictorCastroBR <victorpcpl4y@gmail.com>
2022-04-17 00:46:14 -0300

    Localizar CEP

--- core/views.py
+++ core/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+import re
+from django.shortcuts import redirect, render
+import requests
 
-# Create your views here.
+
+def index(request):
+    if request.method == 'POST':
+        get_cep = request.POST.get('cep')
+        address = requests.get(f'https://viacep.com.br/ws/{get_cep}/json/')
+        address = address.json()
+
+        context = {
+            'cep': address['cep'],
+            'logradouro': address['logradouro'],
+            'complemento': address['complemento'],
+            'localidade': address['localidade'],
+            'uf': address['uf'],
+            'ibge': address['ibge'],
+            'ddd': address['ddd'],
+        }
+
+        return render(request, 'index.html', context)
+
+    else:
+        return render(request, 'index.html')
